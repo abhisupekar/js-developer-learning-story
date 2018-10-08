8f85a823b7f4eec194673285e7e33e90b466dbe4
Nitin Reddy <reddynitin3@gmail.com>
2018-10-08 11:24:52 +0530

    Finding duplicate items from Array

--- array/findDuplicateItems.js
+++ array/findDuplicateItems.js
@@ -1,9 +1,12 @@
-function removeDuplicate(arr) {
+function duplicateElement(arr) {
+    let dupEle = [];
     let newArr = [];
     for(let i = 0; i < arr.length; i++) {
         if(!(newArr.indexOf(arr[i]) > -1)) {
             newArr.push(arr[i]);
+        } else {
+            dupEle.push(arr[i]);
         }
     }
-    return newArr;
+    return dupEle;
 }
